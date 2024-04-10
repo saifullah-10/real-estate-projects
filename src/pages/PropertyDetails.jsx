5a3ebc328a6fbb699a13c9dd6e158dbc26fe05b5
saifullah-10 <saifullah.bus@gmail.com>
2024-04-10 14:04:07 +0600

    update toast functionality

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,9 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ContextProvide } from "../contextProvider/Context";
 import { Link } from "react-router-dom";
-
+import toast, { Toaster } from "react-hot-toast";
 export default function PropertyDetails() {
-  const { propertyDetails } = useContext(ContextProvide);
+  const {
+    propertyDetails,
+    loaderToast,
+    setLoaderToast,
+    loginToast,
+    setLoginToast,
+  } = useContext(ContextProvide);
+
+  useEffect(() => {
+    if (!loaderToast) {
+      toast.success("Successfully Registerd");
+      setLoaderToast(true);
+    } else if (loginToast) {
+      toast.success("Successfully Loged In");
+      setLoginToast(false);
+    }
+  }, [loaderToast, setLoaderToast, loginToast, loginToast]);
+
   return (
     <div>
       {propertyDetails.name}
@@ -12,6 +29,7 @@ export default function PropertyDetails() {
           Go Back To Home
         </button>
       </Link>
+      <Toaster />
     </div>
   );
 }
